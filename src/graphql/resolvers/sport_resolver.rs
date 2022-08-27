46185c204787758865787f1471a7dc10a6cf5cfe
Naohiro Kosuda <nagokos0428@gmail.com>
2022-08-28 06:43:10 +0900

    add sport_resolver.rs

---
+++ src/graphql/resolvers/sport_resolver.rs
@@ -0,0 +1,17 @@
+use async_graphql::{Context, Object, Result};
+
+use crate::database::get_db_pool;
+use crate::graphql::models::sport::{get_sports, Sport};
+
+#[derive(Default)]
+pub struct SportQuery;
+
+#[Object]
+impl SportQuery {
+    #[allow(non_snake_case)]
+    async fn sports(&self, ctx: &Context<'_>) -> Result<Vec<Sport>> {
+        let pool = get_db_pool(ctx).await?;
+        let sports = get_sports(pool).await?;
+        Ok(sports)
+    }
+}
