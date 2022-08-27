8aebe66989233d10b2f915f86b83cde387dbec91
Naohiro Kosuda <nagokos0428@gmail.com>
2022-08-28 06:42:56 +0900

    add tag.rs

---
+++ src/graphql/models/tag.rs
@@ -0,0 +1,38 @@
+use async_graphql::{Object, ID};
+use base64::{encode_config, URL_SAFE};
+use sqlx::PgPool;
+
+#[derive(Clone, Debug, sqlx::FromRow)]
+pub struct Tag {
+    id: i64,
+    name: String,
+}
+
+#[Object]
+impl Tag {
+    async fn id(&self) -> ID {
+        encode_config(format!("Prefecture:{}", self.id), URL_SAFE).into()
+    }
+    async fn name(&self) -> &str {
+        &self.name
+    }
+}
+
+#[tracing::instrument]
+pub async fn get_tags(pool: &PgPool) -> anyhow::Result<Vec<Tag>> {
+    let tags = sqlx::query_as::<_, Tag>(
+        r#"
+        SELECT * FROM tags
+        "#,
+    )
+    .fetch_all(pool)
+    .await;
+
+    match tags {
+        Ok(tags) => Ok(tags),
+        Err(e) => {
+            tracing::error!("get_tags fetch_all error");
+            Err(e.into())
+        }
+    }
+}
