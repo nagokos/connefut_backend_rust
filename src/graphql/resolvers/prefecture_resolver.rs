2b202058046d3eb697eb8a41d16f65ea7670b979
Naohiro Kosuda <nagokos0428@gmail.com>
2022-08-28 06:43:03 +0900

    add prefecture_resolver.rs

---
+++ src/graphql/resolvers/prefecture_resolver.rs
@@ -0,0 +1,17 @@
+use async_graphql::*;
+
+use crate::database::get_db_pool;
+use crate::graphql::models::prefecture::{get_prefectures, Prefecture};
+
+#[derive(Default)]
+pub struct PrefectureQuery;
+
+#[Object]
+impl PrefectureQuery {
+    #[allow(non_snake_case)]
+    async fn prefectures(&self, ctx: &Context<'_>) -> Result<Vec<Prefecture>> {
+        let pool = get_db_pool(ctx).await?;
+        let prefectures = get_prefectures(pool).await?;
+        Ok(prefectures)
+    }
+}
