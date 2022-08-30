04a1b82545da481223d2801bee96708e5403fade
Naohiro Kosuda <nagokos0428@gmail.com>
2022-08-30 11:36:29 +0900

    add migrations

---
+++ db/migrations/000001_create_type_user_role_and_email_verification_status.down.sql
@@ -0,0 +1,2 @@
+DROP TYPE IF EXISTS user_role;
+DROP TYPE IF EXISTS email_verification_status;
---
+++ db/migrations/000001_create_type_user_role_and_email_verification_status.up.sql
@@ -0,0 +1,2 @@
+CREATE TYPE email_verification_status AS ENUM ('pending', 'verified');
+CREATE TYPE user_role AS ENUM ('general', 'admin');
