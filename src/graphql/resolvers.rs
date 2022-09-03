8ce4f58fd056e05b4645279ea78c7d3047c2ae7d
Naohiro Kosuda <nagokos0428@gmail.com>
2022-09-03 22:29:19 +0900

    add Node

--- src/graphql/resolvers.rs
+++ src/graphql/resolvers.rs
@@ -1,3 +1,31 @@
+use async_graphql::*;
+
 pub mod prefecture_resolver;
 pub mod sport_resolver;
 pub mod tag_resolver;
+pub mod user_resolver;
+
+use crate::graphql::models::{prefecture::Prefecture, sport::Sport, tag::Tag, user::User};
+
+#[derive(Interface)]
+#[graphql(field(name = "id", type = "ID"))]
+pub enum Node {
+    Prefecture(Prefecture),
+    Sport(Sport),
+    Tag(Tag),
+    User(User),
+}
+
+#[derive(Default)]
+pub struct RootQuery;
+
+#[Object]
+impl RootQuery {
+    async fn node(&self, _ctx: &Context<'_>, _id: ID) -> Option<Node> {
+        let tag = Tag {
+            id: 9,
+            name: String::from("tag"),
+        };
+        Some(Node::Tag(tag))
+    }
+}
