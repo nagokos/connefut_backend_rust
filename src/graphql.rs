8dbf96b1af02017e2f6e92df871be5d5956f7351
Naohiro Kosuda <nagokos0428@gmail.com>
2022-09-03 22:20:48 +0900

    add mutation

--- src/graphql.rs
+++ src/graphql.rs
@@ -1,17 +1,24 @@
-use async_graphql::{EmptyMutation, EmptySubscription, MergedObject, Schema};
+use async_graphql::{EmptySubscription, MergedObject, Schema};
 use thiserror::Error;
 
 use self::resolvers::{
-    prefecture_resolver::PrefectureQuery, sport_resolver::SportQuery, tag_resolver::TagQuery,
+    prefecture_resolver::PrefectureQuery,
+    sport_resolver::SportQuery,
+    tag_resolver::TagQuery,
+    user_resolver::{UserMutation, UserQuery},
+    RootQuery,
 };
 
 pub mod models;
+pub mod mutations;
 pub mod resolvers;
 
 #[derive(MergedObject, Default)]
-pub struct Query(PrefectureQuery, SportQuery, TagQuery);
+pub struct Query(RootQuery, PrefectureQuery, SportQuery, TagQuery, UserQuery);
+#[derive(MergedObject, Default)]
+pub struct Mutation(UserMutation);
 
-pub type GraphqlSchema = Schema<Query, EmptyMutation, EmptySubscription>;
+pub type GraphqlSchema = Schema<Query, Mutation, EmptySubscription>;
 
 #[derive(Debug, Error)]
 pub enum ApplicationError {
