eacb4f6e596619b04752d573712e0a2cf4dc2452
Naohiro Kosuda <nagokos0428@gmail.com>
2022-09-03 22:23:20 +0900

    schemaに反映されるように

--- src/bin/write_schema.rs
+++ src/bin/write_schema.rs
@@ -1,11 +1,23 @@
 use std::io::Write;
 
-use async_graphql::{EmptyMutation, EmptySubscription, Schema};
-use connefut_api::graphql::Query;
+use async_graphql::{EmptySubscription, Schema};
+use connefut_api::graphql::{
+    mutations::{
+        user_mutation::{RegisterUserInput, RegisterUserResult},
+        Error,
+    },
+    resolvers::Node,
+    Mutation, Query,
+};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let schema = Schema::build(Query::default(), EmptyMutation, EmptySubscription).finish();
+    let schema = Schema::build(Query::default(), Mutation::default(), EmptySubscription)
+        .register_output_type::<Node>()
+        .register_output_type::<RegisterUserResult>()
+        .register_input_type::<RegisterUserInput>()
+        .register_output_type::<Error>()
+        .finish();
     let mut file = std::fs::File::create("schema.graphql")?;
     let contents = &schema.sdl();
     file.write_all(contents.as_bytes())?;
