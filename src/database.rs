3ef31e05386102738617453f1b4dc25e65697dc9
Naohiro Kosuda <nagokos0428@gmail.com>
2022-09-03 22:21:30 +0900

    グローバル変数をconfig.rsで初期化

--- src/database.rs
+++ src/database.rs
@@ -1,44 +1,14 @@
-use anyhow::anyhow;
+use anyhow::{anyhow, Result};
 use async_graphql::Context;
-use once_cell::sync::Lazy;
 use sqlx::{postgres::PgPoolOptions, PgPool};
-use std::{env, sync::Arc};
-struct Config {
-    postgres_host: String,
-    postgres_port: String,
-    postgres_user: String,
-    postgres_password: String,
-    postgres_database: String,
-    postgres_sslmode: String,
-}
+use std::sync::Arc;
 
-impl Config {
-    pub fn database_url(&self) -> String {
-        format!(
-            "postgres://{}:{}@{}:{}/{}?sslmode={}",
-            self.postgres_user,
-            self.postgres_password,
-            self.postgres_host,
-            self.postgres_port,
-            self.postgres_database,
-            self.postgres_sslmode,
-        )
-    }
-}
+use crate::config::Config;
 
-static CONFIG: Lazy<Config> = Lazy::new(|| Config {
-    postgres_host: env::var("POSTGRES_HOST").unwrap(),
-    postgres_user: env::var("POSTGRES_USER").unwrap(),
-    postgres_database: env::var("POSTGRES_DB").unwrap(),
-    postgres_port: env::var("POSTGRES_PORT").unwrap(),
-    postgres_password: env::var("POSTGRES_PASSWORD").unwrap(),
-    postgres_sslmode: env::var("PGSSLMODE").unwrap(),
-});
-
-pub async fn pool() -> anyhow::Result<PgPool> {
+pub async fn pool(config: &Config) -> Result<PgPool> {
     let pool = PgPoolOptions::new()
         .max_connections(20)
-        .connect(&CONFIG.database_url())
+        .connect(config.database.url.as_str())
         .await;
 
     match pool {
