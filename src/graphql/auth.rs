7bb1a63c904e25bd11158e05dcfaf9ca3923f54e
Naohiro Kosuda <nagokos0428@gmail.com>
2022-09-12 21:18:48 +0900

    ログインユーザーをcontextから取得

--- src/graphql/auth.rs
+++ src/graphql/auth.rs
@@ -1,1 +1,13 @@
+use async_graphql::Context;
+
+use crate::graphql::models::user::User;
+
+pub mod cookie;
 pub mod jwt;
+
+pub async fn get_viewer<'ctx>(ctx: &Context<'ctx>) -> &'ctx Option<User> {
+    match ctx.data_opt::<Option<User>>() {
+        Some(viewer) => viewer,
+        None => &None,
+    }
+}
