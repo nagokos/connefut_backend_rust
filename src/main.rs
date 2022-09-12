33647be08f0696078a6a3e822ecbc3dd4eca7435
Naohiro Kosuda <nagokos0428@gmail.com>
2022-09-12 21:20:40 +0900

    リクエスト時にログインユーザーを取得しcontextに入れる

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-use self::graphql::{GraphqlSchema, Mutation, Query};
 use async_graphql::{EmptySubscription, Schema};
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
 use axum::{
-    http::{header, HeaderValue, Method},
+    http::{
+        header::{self, HeaderMap},
+        HeaderValue, Method,
+    },
     routing::post,
     Extension, Router, Server,
 };
+use sqlx::PgPool;
 use std::{sync::Arc, *};
+use tower_http::cors::CorsLayer;
 use tracing_subscriber::fmt::format::FmtSpan;
 
-use tower_http::cors::CorsLayer;
-
+use self::graphql::{GraphqlSchema, Mutation, Query};
+use crate::graphql::auth::{
+    cookie::{get_cookie_from_header, get_value_from_cookie},
+    jwt::get_user_from_token,
+};
 pub mod config;
 mod database;
 mod graphql;
@@ -18,8 +25,22 @@ mod graphql;
 use config::get_config;
 use database::pool;
 
-async fn graphql_handler(schema: Extension<GraphqlSchema>, req: GraphQLRequest) -> GraphQLResponse {
-    schema.execute(req.into_inner()).await.into()
+async fn graphql_handler(
+    Extension(schema): Extension<GraphqlSchema>,
+    req: GraphQLRequest,
+    headers: HeaderMap,
+    Extension(pool): Extension<Arc<PgPool>>,
+) -> GraphQLResponse {
+    let mut req = req.into_inner();
+    if let Some(cookie) = get_cookie_from_header(&headers) {
+        if let Some(token) = get_value_from_cookie(cookie, "token") {
+            let user = get_user_from_token(&pool, token).await;
+            // ctx.data::<Option<User>>でログインユーザにアクセスできる
+            req = req.data(user);
+        }
+    }
+
+    schema.execute(req).await.into()
 }
 
 #[tokio::main]
