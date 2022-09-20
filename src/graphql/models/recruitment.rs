bf75e16a90d12cfd18532def7c3b3df8caee4269
Naohiro Kosuda <nagokos0428@gmail.com>
2022-09-21 00:29:44 +0900

    add recruitment.rs

---
+++ src/graphql/models/recruitment.rs
@@ -0,0 +1,45 @@
+use async_graphql::{Enum, Object, ID};
+use base64::{encode_config, URL_SAFE};
+use chrono::{DateTime, Local};
+
+#[derive(Clone, Debug, sqlx::FromRow)]
+pub struct Recruitment {
+    pub id: i64,
+    pub title: String,
+    pub category: Category,
+    pub venue: Option<String>,
+    pub venue_lat: f64,
+    pub venue_lng: f64,
+    pub start_at: Option<DateTime<Local>>,
+    pub closing_at: Option<DateTime<Local>>,
+    pub detail: Option<String>,
+    pub sport_id: i64,
+    pub prefecture_id: i64,
+    pub status: Status,
+    pub user_id: i64,
+    pub published_at: Option<DateTime<Local>>,
+    pub created_at: DateTime<Local>,
+}
+
+#[Object]
+impl Recruitment {
+    pub async fn id(&self) -> ID {
+        encode_config(format!("Recruitment:{}", self.id), URL_SAFE).into()
+    }
+}
+
+#[derive(Enum, Clone, Copy, Eq, PartialEq, Debug)]
+pub enum Category {
+    Opponent,
+    Personal,
+    Member,
+    Join,
+    Other,
+}
+
+#[derive(Enum, Clone, Copy, Eq, PartialEq, Debug)]
+pub enum Status {
+    Draft,
+    Published,
+    Closed,
+}
