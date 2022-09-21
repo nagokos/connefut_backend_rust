c8518b6c93aef81e0b97fa587cb6cc1a29bcf2e1
Naohiro Kosuda <nagokos0428@gmail.com>
2022-09-22 01:31:46 +0900

    add ids_decode function

--- src/graphql.rs
+++ src/graphql.rs
@@ -1,7 +1,10 @@
-use async_graphql::{EmptySubscription, MergedObject, Schema};
+use anyhow::Result;
+use async_graphql::{EmptySubscription, MergedObject, Schema, ID};
+use base64::{decode_config, URL_SAFE};
 
 use self::resolvers::{
     prefecture_resolver::PrefectureQuery,
+    recruitment_resolver::RecruitmentMutation,
     sport_resolver::SportQuery,
     tag_resolver::TagQuery,
     user_resolver::{UserMutation, UserQuery},
@@ -17,6 +20,22 @@ pub mod resolvers;
 #[derive(MergedObject, Default)]
 pub struct Query(RootQuery, PrefectureQuery, SportQuery, TagQuery, UserQuery);
 #[derive(MergedObject, Default)]
-pub struct Mutation(UserMutation);
+pub struct Mutation(UserMutation, RecruitmentMutation);
 
 pub type GraphqlSchema = Schema<Query, Mutation, EmptySubscription>;
+
+pub fn id_decode(id: &ID) -> Result<i64> {
+    let bytes = decode_config(id.as_bytes(), URL_SAFE)?;
+    let s = String::from_utf8(bytes)?;
+    let split_id: Vec<&str> = s.split(':').collect();
+    let decoded_id: i64 = split_id[1].parse()?;
+    Ok(decoded_id)
+}
+
+pub fn ids_decode(ids: &Vec<ID>) -> Result<Vec<i64>> {
+    let decoded_ids = ids
+        .iter()
+        .filter_map(|id| id_decode(id).ok())
+        .collect::<Vec<i64>>();
+    Ok(decoded_ids)
+}
