f1e687991e224e16ccd178a76f7d0b258fa32c5c
Naohiro Kosuda <nagokos0428@gmail.com>
2022-10-04 23:46:01 +0900

    タグ作成とタグの付与、削除の処理を実装

--- src/graphql/models/tag.rs
+++ src/graphql/models/tag.rs
@@ -59,7 +59,106 @@ pub async fn add_recruitment_tags(
 }
 
 #[tracing::instrument]
-pub async fn get_tags(pool: &PgPool) -> anyhow::Result<Vec<Tag>> {
+pub async fn add_recruitment_tags_tx(
+    tx: &mut Transaction<'_, Postgres>,
+    tag_ids: Vec<i64>,
+    recruitment_id: i64,
+) -> Result<()> {
+    if tag_ids.is_empty() {
+        return Ok(());
+    };
+
+    let sql = "INSERT INTO recruitment_tags(recruitment_id, tag_id, created_at, updated_at) ";
+
+    let mut query_builder = QueryBuilder::<Postgres>::new(sql);
+    query_builder.push_values(tag_ids, |mut b, tag_id| {
+        let now = Local::now();
+        b.push_bind(recruitment_id)
+            .push_bind(tag_id)
+            .push_bind(now)
+            .push_bind(now);
+    });
+    let query = query_builder.build();
+
+    let result = query.execute(tx).await;
+
+    match result {
+        Ok(_) => {
+            tracing::info!("add recruitment tags successed!");
+            Ok(())
+        }
+        Err(e) => {
+            tracing::error!("{:?}", e);
+            tracing::error!("add recruitment tags failed...");
+            Err(e.into())
+        }
+    }
+}
+
+#[tracing::instrument]
+pub async fn remove_recruitment_tags_tx(
+    tx: &mut Transaction<'_, Postgres>,
+    tag_ids: Vec<i64>,
+    recruitment_id: i64,
+) -> Result<()> {
+    if tag_ids.is_empty() {
+        return Ok(());
+    }
+
+    let sql = "DELETE FROM recruitment_tags WHERE (tag_id, recruitment_id) IN";
+
+    let mut query_builder: QueryBuilder<Postgres> = QueryBuilder::new(sql.to_string());
+    query_builder.push_tuples(tag_ids, |mut b, id| {
+        b.push_bind(id).push_bind(recruitment_id);
+    });
+
+    let query = query_builder.build();
+
+    match query.execute(tx).await {
+        Ok(_) => {
+            tracing::info!("remove recruitment tags successed!");
+            Ok(())
+        }
+        Err(e) => {
+            tracing::error!("remove recruitment tags failed...");
+            tracing::error!("{}", e);
+            Err(e.into())
+        }
+    }
+}
+
+#[tracing::instrument]
+pub async fn create(pool: &PgPool, name: &str) -> Result<Tag> {
+    let sql = r#"
+        INSERT INTO tags
+            (name, created_at, updated_at)
+        VALUES
+            ($1, $2, $3)
+        RETURNING *
+    "#;
+
+    let now = Local::now();
+    let row = sqlx::query_as::<_, Tag>(sql)
+        .bind(name)
+        .bind(now)
+        .bind(now)
+        .fetch_one(pool)
+        .await;
+
+    match row {
+        Ok(tag) => {
+            tracing::info!("create tag successed!");
+            Ok(tag)
+        }
+        Err(e) => {
+            tracing::error!("{:?}", e);
+            Err(e.into())
+        }
+    }
+}
+
+#[tracing::instrument]
+pub async fn get_tags(pool: &PgPool) -> Result<Vec<Tag>> {
     let tags = sqlx::query_as::<_, Tag>(
         r#"
         SELECT * FROM tags
