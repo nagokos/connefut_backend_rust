9dda58a98216aebcc9b9218963d0b7021b35150e
Naohiro Kosuda <nagokos0428@gmail.com>
2022-10-04 23:49:04 +0900

    add TagMutation

--- src/graphql/resolvers/tag_resolver.rs
+++ src/graphql/resolvers/tag_resolver.rs
@@ -1,9 +1,29 @@
 use async_graphql::{Context, Object, Result};
+use base64::{encode_config, URL_SAFE};
 
 use crate::{
     database::get_db_pool,
-    graphql::models::tag::{get_tags, Tag},
+    graphql::{
+        models::tag::{self, get_tags, Tag},
+        mutations::tag_mutation::{CreateTagInput, CreateTagResult, CreateTagSuccess},
+    },
 };
+
+#[derive(Debug)]
+pub struct TagEdge {
+    pub cursor: String,
+    pub node: Tag,
+}
+
+#[Object]
+impl TagEdge {
+    pub async fn cursor(&self) -> String {
+        encode_config(format!("Tag:{}", self.node.id), URL_SAFE)
+    }
+    pub async fn node(&self) -> Tag {
+        self.node.clone()
+    }
+}
 
 #[derive(Default)]
 pub struct TagQuery;
@@ -14,5 +34,30 @@ impl TagQuery {
         let pool = get_db_pool(ctx).await?;
         let tags = get_tags(pool).await?;
         Ok(tags)
+    }
+}
+
+#[derive(Default)]
+pub struct TagMutation;
+
+#[Object]
+impl TagMutation {
+    async fn create_tag(
+        &self,
+        ctx: &Context<'_>,
+        input: CreateTagInput,
+    ) -> Result<CreateTagResult> {
+        let pool = get_db_pool(ctx).await?;
+
+        if let Some(error) = input.validate_is_already_tag_name(pool).await? {
+            return Ok(error.into());
+        }
+
+        let tag = tag::create(pool, &input.name).await?;
+        let tag_edge = TagEdge {
+            cursor: String::default(),
+            node: tag,
+        };
+        Ok(CreateTagSuccess { tag_edge }.into())
     }
 }
