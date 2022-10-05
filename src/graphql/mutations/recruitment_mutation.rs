2cdba051dc9c1bd424d46ec6c6f1cc1600f903a6
Naohiro Kosuda <nagokos0428@gmail.com>
2022-10-05 22:52:16 +0900

    不要なコードを削除

--- src/graphql/mutations/recruitment_mutation.rs
+++ src/graphql/mutations/recruitment_mutation.rs
@@ -1,6 +1,6 @@
 use async_graphql::{Enum, InputObject, SimpleObject, Union, ID};
 use chrono::{DateTime, Local};
-use validator::{Validate, ValidationError};
+use validator::Validate;
 
 use crate::graphql::{
     models::recruitment::{Category, Status},
