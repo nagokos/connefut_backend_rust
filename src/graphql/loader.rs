0a0176e67ea4ab9135e37992d3e4d093b240312a
Naohiro Kosuda <nagokos0428@gmail.com>
2022-10-21 16:44:15 +0900

    add stock loader

--- src/graphql/loader.rs
+++ src/graphql/loader.rs
@@ -3,10 +3,14 @@ use std::sync::Arc;
 use async_graphql::dataloader::DataLoader;
 use sqlx::{Pool, Postgres};
 
-use self::{prefecture::PrefectureLoader, sport::SportLoader, tag::TagLoader, user::UserLoader};
+use self::{
+    prefecture::PrefectureLoader, sport::SportLoader, stock::StockLoader, tag::TagLoader,
+    user::UserLoader,
+};
 
 pub mod prefecture;
 pub mod sport;
+pub mod stock;
 pub mod tag;
 pub mod user;
 
@@ -15,6 +19,7 @@ pub struct Loaders {
     pub tag_loader: DataLoader<TagLoader>,
     pub prefecture_loader: DataLoader<PrefectureLoader>,
     pub sport_loader: DataLoader<SportLoader>,
+    pub stock_loader: DataLoader<StockLoader>,
 }
 
 impl Loaders {
@@ -24,12 +29,14 @@ impl Loaders {
         let prefecture_loader =
             DataLoader::new(PrefectureLoader { pool: pool.clone() }, tokio::spawn);
         let sport_loader = DataLoader::new(SportLoader { pool: pool.clone() }, tokio::spawn);
+        let stock_loader = DataLoader::new(StockLoader { pool: pool.clone() }, tokio::spawn);
 
         Self {
             user_loader,
             tag_loader,
             prefecture_loader,
             sport_loader,
+            stock_loader,
         }
     }
 }
