9232b9f947acf9399c3274d263c35b78eaed94cf
Naohiro Kosuda <nagokos0428@gmail.com>
2022-10-28 21:46:55 +0900

    add following_loader

--- src/graphql/loader.rs
+++ src/graphql/loader.rs
@@ -1,11 +1,14 @@
 use std::sync::Arc;
 
-use async_graphql::dataloader::DataLoader;
+use async_graphql::{dataloader::DataLoader, Context};
 use sqlx::{Pool, Postgres};
 
 use self::{
-    prefecture::PrefectureLoader, sport::SportLoader, stock::StockLoader, tag::TagLoader,
-    user::UserLoader,
+    prefecture::PrefectureLoader,
+    sport::SportLoader,
+    stock::StockLoader,
+    tag::TagLoader,
+    user::{FollowingLoader, UserLoader},
 };
 
 pub mod prefecture;
@@ -16,6 +19,7 @@ pub mod user;
 
 pub struct Loaders {
     pub user_loader: DataLoader<UserLoader>,
+    pub following_loader: DataLoader<FollowingLoader>,
     pub tag_loader: DataLoader<TagLoader>,
     pub prefecture_loader: DataLoader<PrefectureLoader>,
     pub sport_loader: DataLoader<SportLoader>,
@@ -25,6 +29,8 @@ pub struct Loaders {
 impl Loaders {
     pub fn new(pool: &Arc<Pool<Postgres>>) -> Self {
         let user_loader = DataLoader::new(UserLoader { pool: pool.clone() }, tokio::spawn);
+        let following_loader =
+            DataLoader::new(FollowingLoader { pool: pool.clone() }, tokio::spawn);
         let tag_loader = DataLoader::new(TagLoader { pool: pool.clone() }, tokio::spawn);
         let prefecture_loader =
             DataLoader::new(PrefectureLoader { pool: pool.clone() }, tokio::spawn);
@@ -33,6 +39,7 @@ impl Loaders {
 
         Self {
             user_loader,
+            following_loader,
             tag_loader,
             prefecture_loader,
             sport_loader,
