49446b8398dc3296f1e0e6a7154e8d3cbdcbe9c1
Naohiro Kosuda <nagokos0428@gmail.com>
2022-11-01 02:58:40 +0900

    add loader

--- src/graphql/loader/stock.rs
+++ src/graphql/loader/stock.rs
@@ -9,29 +9,77 @@ pub struct StockLoader {
 }
 
 #[async_trait]
+impl Loader<i64> for StockLoader {
+    type Value = i64;
+    type Error = Arc<sqlx::Error>;
+
+    // 募集がストックされている数
+    async fn load(&self, keys: &[i64]) -> Result<HashMap<i64, Self::Value>, Self::Error> {
+        let sql = "SELECT recruitment_id, COUNT(DISTINCT id) FROM stocks WHERE recruitment_id IN (";
+        let mut query_builder = QueryBuilder::<Postgres>::new(sql);
+        let mut separated = query_builder.separated(",");
+        for key in keys.iter() {
+            separated.push_bind(key);
+        }
+        separated.push_unseparated(") ");
+        query_builder.push("GROUP BY recruitment_id");
+        let query = query_builder.build();
+        let result = query.fetch_all(&*self.pool).await;
+
+        match result {
+            Ok(rows) => {
+                tracing::info!("StockLoader load stocked_count successed!!");
+                let stock_hash: HashMap<i64, i64> = rows
+                    .iter()
+                    .map(|row| {
+                        let recruitment_id: i64 = row.get("recruitment_id");
+                        let count: i64 = row.get("count");
+                        (recruitment_id, count)
+                    })
+                    .collect();
+                Ok(stock_hash)
+            }
+            Err(e) => {
+                tracing::error!("StockLoader load stocked_count failed: {:?}", e);
+                Err(e.into())
+            }
+        }
+    }
+}
+
+#[async_trait]
 impl Loader<[i64; 2]> for StockLoader {
     type Value = ();
     type Error = Arc<sqlx::Error>;
 
+    // keysは[user_id, recruitment_id]の形で送られてくる
+    // ユーザーが募集をストックしているか
     async fn load(&self, keys: &[[i64; 2]]) -> Result<HashMap<[i64; 2], Self::Value>, Self::Error> {
-        println!("{:?}", keys);
-        let sql = "SELECT * FROM stocks WHERE (user_id, recruitment_id) IN";
-        let mut query_builder: QueryBuilder<Postgres> = QueryBuilder::new(sql);
+        let sql = "SELECT user_id, recruitment_id FROM stocks WHERE (user_id, recruitment_id) IN";
+        let mut query_builder = QueryBuilder::<Postgres>::new(sql);
         query_builder.push_tuples(keys, |mut b, key| {
             b.push_bind(key[0]).push_bind(key[1]);
         });
         let query = query_builder.build();
-        let rows = query.fetch_all(&*self.pool).await?;
-        // { [user_id, recruitment_id]: () }の形に整形する
-        // load_oneはOption型を返すので組み合わせがあればviwerHasStockedをtrueにする
-        let stock_hash: HashMap<[i64; 2], ()> = rows
-            .iter()
-            .map(|row| {
-                let user_id: i64 = row.get("user_id");
-                let recruitment_id: i64 = row.get("recruitment_id");
-                ([user_id, recruitment_id], ())
-            })
-            .collect();
-        Ok(stock_hash)
+        let result = query.fetch_all(&*self.pool).await;
+        match result {
+            Ok(rows) => {
+                tracing::info!("RecruitmentLoader load viewer_has_stocked successed!!");
+                // {[user_id, recruitment_id], ()}の形に整形する
+                let recruitment_hash: HashMap<[i64; 2], ()> = rows
+                    .iter()
+                    .map(|row| {
+                        let user_id: i64 = row.get("user_id");
+                        let recruitment_id: i64 = row.get("recruitment_id");
+                        ([user_id, recruitment_id], ())
+                    })
+                    .collect();
+                Ok(recruitment_hash)
+            }
+            Err(e) => {
+                tracing::error!("RecruitmentLoader load viewer_has_stocked failed: {:?}", e);
+                Err(e.into())
+            }
+        }
     }
 }
